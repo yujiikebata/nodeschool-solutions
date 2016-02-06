f9b43cc560324352e559538ae09be8fd240fef75
yujiikebata <yujiikebata@gmail.com>
2016-02-06 19:39:58 +0900

    modified count-to-6: arrow-functions-part-1 (add official solution)

--- count-to-6/arrow-functions-part-1.js
+++ count-to-6/arrow-functions-part-1.js
@@ -3,5 +3,11 @@ let inputs = process.argv.slice(2);
 
 // pick up their first character
 let firstChars = inputs.map(item => item.substr(0, 1));
+// concatenate firstChars
 let result = firstChars.reduce((prev, curr) => prev += curr);
 console.log(`[${inputs}] becomes "${result}"`);
+
+//------ official solution --------
+var inputs = process.argv.slice(2);
+var result = inputs.map(s => s[0]).reduce((sofar, s) => sofar + s);
+console.log(`[${inputs} becomes "${result}"`]);
