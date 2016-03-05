8a3d342aec75c24245e0f6ef4b3f324835fe33ec
yujiikebata <yujiikebata@gmail.com>
2016-03-05 16:43:56 +0900

    solve count-to-6: iterator-for-of

--- tower-of-babel/iterator-for-of.js
+++ tower-of-babel/iterator-for-of.js
@@ -2,7 +2,22 @@
 const max = process.argv[2];
 let FizzBuzz = {
   [Symbol.iterator]() {
-    
+    let num = 0, result = 1;
+    return {
+      next() {
+        num += 1;
+        result = num;
+        if (num % 3 === 0) result = 'Fizz';
+        if (num % 5 === 0) result = 'Buzz';
+        if (num % 3 === 0 && num % 5 === 0) result = 'FizzBuzz';
+        if (num <= max) {
+          return { done: false, value: result };
+        } else {
+          return { done: true, value: result };
+        }
+
+      }
+    }
   }
 };
 
